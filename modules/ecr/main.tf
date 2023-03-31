83eb3d9a06adb4dcfb220e3aec1765a4cac50b65
rahulkulmi <rahul@newput.com>
2023-03-31 15:22:59 +0530

    Added changes for ecr images.

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -2,6 +2,7 @@ resource "aws_ecr_repository" "ecr" {
   for_each             = toset(var.ecr_name)
   name                 = each.key
   image_tag_mutability = var.image_tag_mutability
+  force_delete         = true # drop ECR repo if it is not empty.
 
   image_scanning_configuration {
     scan_on_push = var.scan_on_push
