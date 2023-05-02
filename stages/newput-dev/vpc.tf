857a30ef2547f72177477c357e70e3a4b7792e60
rahulkulmi <rahul@newput.com>
2023-05-02 11:41:10 +0530

    Added changes for testing-1.

--- stages/newput-dev/vpc.tf
+++ stages/newput-dev/vpc.tf
@@ -98,7 +98,7 @@ resource "aws_nat_gateway" "gw" {
   # count = var.az_count
   # subnet_id     = element(aws_subnet.public.*.id, count.index)
   # allocation_id = element(aws_eip.gw.*.id, count.index)
-  subnet_id     = aws_subnet.public.id
+  subnet_id     = aws_subnet.public.*.id
   allocation_id = aws_eip.gw.id
 
   tags = merge(local.tags, {
