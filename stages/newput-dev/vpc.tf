5f6f10e7d1decc34445c48718009eb222f632da5
rahulkulmi <rahul@newput.com>
2023-05-02 11:19:35 +0530

    Added changes for testing.

--- stages/newput-dev/vpc.tf
+++ stages/newput-dev/vpc.tf
@@ -85,7 +85,7 @@ resource "aws_route" "internet_access" {
 
 # Create a NAT gateway with an Elastic IP for each private subnet to get internet connectivity
 resource "aws_eip" "gw" {
-  count      = var.az_count
+  # count      = var.az_count
   vpc        = true
   depends_on = [aws_internet_gateway.gw]
 
@@ -95,7 +95,7 @@ resource "aws_eip" "gw" {
 }
 
 resource "aws_nat_gateway" "gw" {
-  count = var.az_count
+  # count = var.az_count
   # subnet_id     = element(aws_subnet.public.*.id, count.index)
   # allocation_id = element(aws_eip.gw.*.id, count.index)
   subnet_id     = aws_subnet.public.id
@@ -108,7 +108,7 @@ resource "aws_nat_gateway" "gw" {
 
 # Create a new route table for the private subnets, make it route non-local traffic through the NAT gateway to the internet
 resource "aws_route_table" "private" {
-  count  = var.az_count
+  # count  = var.az_count
   vpc_id = aws_vpc.main.id
 
   route {
@@ -124,7 +124,7 @@ resource "aws_route_table" "private" {
 
 # Explicitly associate the newly created route tables to the private subnets (so they don't default to the main route table)
 resource "aws_route_table_association" "private" {
-  count = var.az_count
+  # count = var.az_count
   # subnet_id      = element(aws_subnet.private.*.id, count.index)
   # route_table_id = element(aws_route_table.private.*.id, count.index)
   subnet_id      = aws_subnet.private.id
