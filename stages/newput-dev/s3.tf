1305dcd175c112278ca13c73e65de3ab53d7865d
rahulkulmi <rahul@newput.com>
2023-05-02 10:36:02 +0530

    Added changes for s3 bucket control.

--- stages/newput-dev/s3.tf
+++ stages/newput-dev/s3.tf
@@ -8,6 +8,13 @@ resource "aws_s3_bucket" "newput_react_web" {
   })
 }
 
+resource "aws_s3_bucket_ownership_controls" "newput_react_web_ownership_controls" {
+  bucket = aws_s3_bucket.newput_react_web.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "newput_react_web_bucket_acl" {
   bucket = aws_s3_bucket.newput_react_web.id
   acl    = "private"
